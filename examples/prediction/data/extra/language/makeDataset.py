2ab677926555fddb86b791081cacbcc8fbfcfe0d
Chetan Surpur <chetan51@gmail.com>
2013-07-29 01:40:30 -0700

    Renamed variable

--- examples/prediction/data/extra/language/makeDataset.py
+++ examples/prediction/data/extra/language/makeDataset.py
@@ -1,14 +1,14 @@
 import re
 
-big = file('big.txt').read()
-big = re.sub(' +', ' ', big)
-N = len(big)
+data = file('betty.txt').read()
+data = re.sub(' +', ' ', data)
+N = len(data)
 
 print "letter"
 print "string"
 print ""
 
 for i in xrange(0, N):
-    c = big[i]
+    c = data[i]
     if ord(c) > 31 and ord(c) < 127:
         print c
